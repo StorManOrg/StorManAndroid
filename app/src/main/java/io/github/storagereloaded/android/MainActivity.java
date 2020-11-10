9a0b2ed35fe38b631fc62bb9cead25f44682e87a
TecCheck <mcteccy@gmail.com>
2020-11-10 17:56:24 +0100

    Add basic list

--- app/src/main/java/io/github/storagereloaded/android/MainActivity.java
+++ app/src/main/java/io/github/storagereloaded/android/MainActivity.java
@@ -1,14 +1,27 @@
 package io.github.storagereloaded.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), manager.getOrientation()));
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(new RecyclerViewAdapter());
     }
 }
